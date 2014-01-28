dc5149f9d15e23ed06a000e18ef89e86e155a39e
ajk4 <jakubowski.adam@gmail.com>
2014-01-28 17:48:18 +0100

    +nowe naglowki +flaga open +testy

--- testy.c
+++ testy.c
@@ -1,13 +1,43 @@
 #include "fs_lib.h"
+#include "stdio.h"
+
+#define DEBUG printf("wat\n"); fflush(stdout);
+
+int test_write() {
+  int srvhndl, fd, i;
+  char *buffer = "wololo"; 
+
+  fs_openserver("127.0.0.1", "TCP", 9000, &srvhndl);
+  fs_open(srvhndl, "test3.txt", &fd);
+  DEBUG
+  fs_write(srvhndl, fd, buffer,5);
+  fs_close(srvhndl, fd);
+  DEBUG
+  for(i=0; i<5; i++){
+    printf("%c", buffer[i]);
+  }
+  printf("\n");
+  fflush(stdout);
+}
+
+int test_read() {
+  int srvhndl, fd, i;
+  char buffer[1024];
+
+  fs_openserver("127.0.0.1", "TCP", 9001, &srvhndl);
+  fs_open(srvhndl, "test3.txt", &fd);
+  DEBUG
+  fs_read(srvhndl, fd, buffer,5);
+  DEBUG
+  for(i=0; i<5; i++){
+    printf("%c", buffer[i]);
+    fflush(stdout);
+  }
+  printf("\n");
+  fflush(stdout);
+}
 
 int main(int argc, char **argv){
-  int srvhndl, fd;
-  if(fs_openserver("127.0.0.1", "TCP", 9001, &srvhndl) != 0){
-    return -1;
-  }
-  if(fs_open(srvhndl, "test.txt", &fd) != 0){
-    return -1;
-  }
- 
+  test_read();
   return 0;
 }
